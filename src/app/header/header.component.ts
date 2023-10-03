e856e056752345b2d6b533bd7ee4a20746bca840
ketan-chauhan997 <chauhan.25997@gmail.com>
2023-10-03 18:05:56 +0530

    Logout and Routing from Nav Bar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,12 +9,16 @@ import { Route, Router } from '@angular/router';
 export class HeaderComponent {
   constructor(private route:Router){}
   navBarType:string='default';
+  sellerName:string='';
   ngOnInit():void{
     this.route.events.subscribe((value:any)=>{
       if(value.url){
         if(localStorage.getItem('seller') && value.url.includes('seller')){
           console.warn("Inside seller area");
           this.navBarType='seller'
+          let sellerVar= localStorage.getItem('seller');
+          let sellerData= sellerVar && JSON.parse(sellerVar)[0];
+          this.sellerName=sellerData.name;
         }
         else{
           console.warn('Outside Seller');
@@ -22,5 +26,9 @@ export class HeaderComponent {
         }
       }
     })
+  }
+  clearLocalStorage():void{
+    localStorage.removeItem('seller');
+    this.route.navigate(['/']); 
   }
 }
