e0777ff6aae4c1b3d0884fdf97b5fdb674396127
ketan-chauhan997 <chauhan.25997@gmail.com>
2023-10-03 17:44:29 +0530

    Nav bar Changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  constructor(private route:Router){}
+  navBarType:string='default';
+  ngOnInit():void{
+    this.route.events.subscribe((value:any)=>{
+      if(value.url){
+        if(localStorage.getItem('seller') && value.url.includes('seller')){
+          console.warn("Inside seller area");
+          this.navBarType='seller'
+        }
+        else{
+          console.warn('Outside Seller');
+          this.navBarType='default';
+        }
+      }
+    })
+  }
 }
